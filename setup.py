abf3ba0b4a8b1abc93dcadde6546f8435e9d2dc5
Avery Fay <avery@shadypixel.com>
2013-11-30 03:49:02 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+import object_pool
+
+setup(name='object_pool',
+      version=object_pool.__version__,
+      url='https://github.com/btmorex/object_pool',
+      license='MIT',
+      author='Avery Fay',
+      author_email='avery@shadypixel.com',
+      packages=find_packages(exclude=['object_pool.test']),
+      test_suite='object_pool.test',
+)
